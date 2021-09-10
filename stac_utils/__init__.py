d6099c79ac34a63e8c0a9489dfcbe91dc3db1b3a
Micheál Keane <ffaristocrat@gmail.com>
2021-09-10 11:24:15 -0700

    first function and basic layout

---
+++ stac_utils/__init__.py
@@ -0,0 +1,27 @@
+def listify(
+    string: str,
+    type_: type = str,
+    sep: str = ",",
+    ignore_errors: bool = False,
+    ignore_empty: bool = True,
+) -> list:
+    """ Split a string into a list, converting to a type as necessary """
+
+    if not string.strip():
+        return []
+
+    items = [s.strip() for s in string.split(sep) if not ignore_empty or s.strip()]
+
+    if ignore_errors:
+        finished = []
+        for i in items:
+            try:
+                finished.append(type_(i))
+            except (TypeError, ValueError):
+                if not ignore_empty:
+                    finished.append(None)
+
+    else:
+        finished = [type_(i) if not ignore_empty or i else None for i in items]
+
+    return finished
