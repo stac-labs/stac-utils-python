b3841508c77f3a0c631d219c54e497751d5b1f15
Micheál Keane <ffaristocrat@gmail.com>
2022-07-26 13:37:03 -0700

    convert to snake case tests

--- src/tests/test_base_utils.py
+++ src/tests/test_base_utils.py
@@ -140,6 +140,31 @@ class TestBaseUtils(unittest.TestCase):
         with secrets(dictionary=test_dict):
             pass
 
+    def test_convert_to_snake_case_string(self):
+        self.assertEquals(convert_to_snake_case("FooBar"), "foo_bar")
+        self.assertEquals(convert_to_snake_case("foo_bar"), "foo_bar")
+        self.assertEquals(convert_to_snake_case("FOOBar"), "foo_bar")
+        self.assertEquals(convert_to_snake_case("fooBar"), "foo_bar")
+        self.assertEquals(convert_to_snake_case("FooBar"), "foo_bar")
+        self.assertEquals(convert_to_snake_case("Spam"), "spam")
+        self.assertEquals(convert_to_snake_case("spam"), "spam")
+        self.assertEquals(convert_to_snake_case(""), "")
+
+    def test_convert_to_snake_case_list(self):
+        self.assertEquals(convert_to_snake_case(["FooBar"]), ["foo_bar"])
+        self.assertEquals(convert_to_snake_case(["FooBar", "FooBar"]), ["foo_bar", "foo_bar"])
+        self.assertEquals(convert_to_snake_case(["Spam"]), ["spam"])
+        self.assertEquals(convert_to_snake_case([""]), [""])
+        self.assertEquals(convert_to_snake_case([]), [])
+
+    def test_convert_to_snake_case_dict(self):
+        self.assertEquals(convert_to_snake_case({"FooBar": "FooBar"}), {"foo_bar": "FooBar"})
+        self.assertEquals(convert_to_snake_case({"FooBar": "FooBar", "SpamBar": True}), {"foo_bar": "FooBar", "spam_bar": True})
+
+    def test_convert_to_snake_case_mixed(self):
+        self.assertEquals(convert_to_snake_case([{"FooBar": "FooBar"}]), [{"foo_bar": "FooBar"}])
+        self.assertEquals(convert_to_snake_case({"Spam": ["FooBar", "FooBar"]}), {"spam": ["foo_bar", "foo_bar"]})
+
 
 if __name__ == "__main__":
     unittest.main()
