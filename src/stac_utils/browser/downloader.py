b178e6af7176d0521cafc707d7776966878ee7f3
Micheál Keane <ffaristocrat@gmail.com>
2022-08-08 11:44:47 -0700

    Downloader and driver

---
+++ src/stac_utils/browser/downloader.py
@@ -0,0 +1,64 @@
+import glob
+import os
+import time
+from collections import defaultdict
+from typing import Iterator
+
+
+class Downloader:
+    """ Iterator that looks for files in a directory and waits for one
+        to finish downloading.
+
+        Later calls will not return files that have already been given.
+
+        Useful
+
+    """
+    def __init__(self, directory: str, polling: float = 10.0, pattern: str = None):
+        self.directory = directory
+        self.returned_files = set()
+        self.polling = polling
+        self.pattern = pattern
+
+    def clear(self):
+        self.returned_files.clear()
+
+    def check_for_new_files(self, pattern: str = None) -> Iterator[str]:
+        print("Checking for new files")
+        pattern = pattern if pattern is not None else self.pattern
+        last_size = defaultdict(int)
+        finished = set()
+        while True:
+            time.sleep(self.polling)
+            directory_files = glob.glob(os.path.join(self.directory, pattern))
+
+            if not directory_files:
+                """ No files so let's wait """
+                continue
+
+            downloaded = [
+                file for file in
+                directory_files
+                if file not in self.returned_files
+            ]
+            if not downloaded:
+                """ If there are files and we've already returned them all,
+                    stop iterating
+                """
+                break
+
+            for download in downloaded:
+                """ Check each file, compare file size to last time it was checked
+                    and if it's been static, assume it's finished.
+                """
+                size = os.path.getsize(download)
+                if (last_size[download] > 0) and (size == last_size[download]):
+                    finished.add(download)
+                elif size > 0:
+                    last_size[download] = size
+
+            for filename in finished:
+                """ Start spitting out finished files """
+                print(f"{filename} is complete")
+                self.returned_files.add(filename)
+                yield filename
---
+++ src/stac_utils/browser/driver.py
@@ -0,0 +1,47 @@
+import tempfile
+
+from selenium.webdriver import ChromeOptions
+from undetected_chromedriver._compat import Chrome
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+class ChromeDriver:
+    """ Chrome drive sugar that loads in some intelligent defaults for setting up
+        a headless version for scripts.
+
+        By default, it'll use a temporary directory for downloaded files.
+    """
+    def __init__(self, download_directory: str = None, is_headless: bool = True):
+        self.temp_dir = None
+        self.download_directory = download_directory
+        self.is_headless = is_headless
+        self.driver = None
+
+    def __enter__(self):
+        if self.download_directory is None:
+            self.temp_dir = tempfile.TemporaryDirectory()
+            self.download_directory = self.temp_dir.name
+
+        options = ChromeOptions()
+        options.add_experimental_option(
+            "prefs", {"download.default_directory": self.download_directory}
+        )
+        options.add_argument("--disable-extensions")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--no-sandbox")
+        if self.is_headless:
+            options.add_argument("--headless")
+
+        path = ChromeDriverManager().install()
+
+        self.driver = Chrome(options=options, executable_path=path)
+        self.driver.download_directory = self.download_directory
+
+        return self.driver
+
+    def __exit__(self, *args):
+        if self.driver:
+            self.driver.close()
+
+        if self.temp_dir:
+            self.temp_dir.cleanup()
