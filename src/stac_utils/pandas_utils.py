fb3f0d464c07ff3456befeab05061c86293ab0e6
bxjw <bella@staclabs.io>
2022-09-29 14:06:32 -0400

    add dataframe utils

---
+++ src/stac_utils/pandas_utils.py
@@ -0,0 +1,23 @@
+from pandas import DataFrame
+from typing import List
+
+from .google import send_data_to_sheets
+
+def send_dataframe_to_sheets(
+    df: DataFrame,
+    spreadsheet_id: str,
+    range: str,
+    input_option: str = 'RAW',
+    client = None
+) -> dict:
+    """Posts the DataFrame to the Google Sheet and returns the API response"""
+
+    data = transform_to_lists(df)
+    return send_data_to_sheets(data, spreadsheet_id, range, input_option, client)
+
+def transform_to_lists(df: DataFrame) -> List[list]:
+    df = df.fillna("")
+    row_data = [[col for col in df.columns]]
+    for row in df.itertuples(index=False):
+        row_data.append([str(col) for col in row])
+    return row_data
