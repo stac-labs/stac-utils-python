62cf3fec19955d4aebb5930669c03f1f2bf40a0c
bxjw <bella@staclabs.io>
2022-10-26 11:23:02 -0400

    add secret factory

--- src/stac_utils/__init__.py
+++ src/stac_utils/__init__.py
@@ -2,3 +2,4 @@ from .listify import listify
 from .secret_context import secrets
 from .convert import convert_to_snake_case
 from .truthy import truthy
+from .secret_factory import SecretsJSONFactory
