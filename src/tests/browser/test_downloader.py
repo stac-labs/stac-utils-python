451be0c91a77f7ded17b586e1e328bf063ab478f
Micheál Keane <ffaristocrat@gmail.com>
2022-11-30 08:00:43 -0800

    browser skeleton

---
+++ src/tests/browser/test_downloader.py
@@ -0,0 +1,21 @@
+import unittest
+
+from src.stac_utils.browser.downloader import Downloader
+
+
+class TestDownloader(unittest.TestCase):
+    def setUp(self):
+        """Setup temporary directory with temp files to examine"""
+
+    def test_init(self):
+        """Test init"""
+
+    def test_clear(self):
+        """Test clear"""
+
+    def test_check_for_new_files(self):
+        """Test check for new files"""
+
+    def test_check_for_new_files_all_finished(self):
+        """Test check for new files when we've consumed all files"""
+
