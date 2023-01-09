03d9dd1e823a438208b2ffbb0c911c9b57fed337
Bella Wang <bella.xj.wang@gmail.com>
2023-01-09 09:27:24 -0500

    allow mailgun template (#31)
    * allow mailgun template
    
    * tests
    
    * let's see if this gets it working
    
    Co-authored-by: Micheál Keane <ffaristocrat@gmail.com>
    Co-authored-by: bxjw <bella@staclabs.io>

--- src/tests/test_email.py
+++ src/tests/test_email.py
@@ -6,9 +6,16 @@ from src.stac_utils.email import Emailer
 class TestEmailer(unittest.TestCase):
     def test_init(self):
         """Test init method"""
+        Emailer(
+            "mail.staclabs.io",
+            "API_KEY",
+            "engineering@staclabs.io",
+            "engineering@staclabs.io",
+        )
 
     def test_init_no_keys(self):
         """Test init uses keys from environment"""
+        self.assertRaises(KeyError, lambda: Emailer())
 
     def test_send_email(self):
         """Test send email"""
