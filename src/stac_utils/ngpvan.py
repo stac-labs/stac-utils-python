de908e07a390678e692aa695a22ecb23f2152ece
Bella Wang <bella.xj.wang@gmail.com>
2023-03-23 18:01:23 -0400

    Pagination (#37)
    * paginate correctly
    
    * how about now
    
    ---------
    
    Co-authored-by: bxjw <bella@staclabs.io>

--- src/stac_utils/ngpvan.py
+++ src/stac_utils/ngpvan.py
@@ -99,11 +99,11 @@ class NGPVANClient(HTTPClient):
         all_items = []
         next_url = url
         while next_url:
-            print(f"Getting {url}")
-            data = self.get(url, **kwargs)
-            if "items" not in data:
+            print(f"Getting {next_url}")
+            data = self.get(next_url, **kwargs)
+            if "items" not in data or len(data.get("items")) == 0:
                 return all_items
             all_items.extend(data["items"])
 
-            next_url = data.get("next_page_link")
+            next_url = data.get("next_page_link").split("/")[-1]
         return all_items
