74a11c9d9e77b8721a87827f5f376aa691a2224c
Micheál Keane <ffaristocrat@gmail.com>
2023-04-03 13:25:33 -0700

    check env timezone on function call rather than on import

--- src/stac_utils/time.py
+++ src/stac_utils/time.py
@@ -5,19 +5,25 @@ from dateutil.utils import default_tzinfo, today as _today
 from dateutil.parser import parse as _parse
 from dateutil.tz import gettz
 
-timezone = gettz(os.environ.get("TIMEZONE", "America/New_York"))
+DEFAULT_TIMEZONE = "America/New_York"
 
 
 def parse_date_for_tz(value: str) -> datetime:
     """Parse date adjusting for timezone if timezone is not present"""
+    timezone = gettz(os.environ.get("TIMEZONE", DEFAULT_TIMEZONE))
+
     return default_tzinfo(_parse(value), timezone)
 
 
 def get_today_tz() -> datetime:
     """Shortcut for getting today's date in the timezone specified by the 'TIMEZONE' environment variable"""
+    timezone = gettz(os.environ.get("TIMEZONE", DEFAULT_TIMEZONE))
+
     return _today(timezone)
 
 
 def get_now_tz() -> datetime:
     """Shortcut for getting the current time in the timezone specified by the 'TIMEZONE' environment variable"""
+    timezone = gettz(os.environ.get("TIMEZONE", DEFAULT_TIMEZONE))
+
     return datetime.now(timezone)
