ba5fa6bcd2657a83cd943f77956c7dc514425b91
Micheál Keane <ffaristocrat@gmail.com>
2023-04-04 22:48:14 -0700

    black

--- src/stac_utils/ticker_request.py
+++ src/stac_utils/ticker_request.py
@@ -16,17 +16,17 @@ class TickerAuthException(TickerException):
 
 
 class TickerRequest(HTTPClient):
-    """ Usage:
-        from stac_utils.ticker_request import TickerRequest
-        from stac_utils import secrets # only necessary in AWS
+    """Usage:
+    from stac_utils.ticker_request import TickerRequest
+    from stac_utils import secrets # only necessary in AWS
 
-        # with secrets() context only needed in AWS
-        with secrets(secret_name = os.environ['TICKER_SECRET_NAME']):
-            ticker = TickerRequest()
-            ticker.add_data('FL', 'AWS Lambda', 'event-sync', 'events created', 155)
-            ticker.add_data('FL', 'AWS Lambda', 'event-sync', 'signups created', 1342)
+    # with secrets() context only needed in AWS
+    with secrets(secret_name = os.environ['TICKER_SECRET_NAME']):
+        ticker = TickerRequest()
+        ticker.add_data('FL', 'AWS Lambda', 'event-sync', 'events created', 155)
+        ticker.add_data('FL', 'AWS Lambda', 'event-sync', 'signups created', 1342)
 
-            ticker.send_to_ticker()
+        ticker.send_to_ticker()
     """
 
     def __init__(self, *args, **kwargs):
