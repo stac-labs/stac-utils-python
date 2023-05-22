15756c35506c9692a523cd87cb59975fd17ac572
Micheál Keane <ffaristocrat@gmail.com>
2023-05-22 14:21:30 -0700

    sketch out tests

--- src/tests/test_google.py
+++ src/tests/test_google.py
@@ -19,8 +19,8 @@ from src.stac_utils.google import (
 
 
 class TestGoogle(unittest.TestCase):
-    def test_get_credentials(self):
-        """Test credentials are created successfully"""
+    def test_get_credentials_json_blob(self):
+        """Test credentials are created successfully from blob"""
 
     def test_get_credentials_environment(self):
         """Test credentials are created successfully from environment"""
@@ -28,17 +28,47 @@ class TestGoogle(unittest.TestCase):
     def test_get_credentials_missing(self):
         """Test credentials fails because no credentials were provided"""
 
-    def test_auth_bq(self):
+    def test_get_client(self):
         """Test it gets credentials and makes a client"""
+
+    def test_get_client_auto_credential(self):
+        """Test it creates a client from default credentials"""
+
+    def test_auth_gcs(self):
+        """Test it makes a gcs client"""
+
+    def test_auth_gcs_other_scopes(self):
+        """Test auth gcs with custom scopes"""
+
+    def test_auth_bq(self):
+        """Test it makes a bigquery client"""
+
+    def test_auth_bq_other_scopes(self):
+        """Test auth bq with custom scopes"""
+
+    def test_auth_gmail(self):
+        """Test it gets credentials & builds a gmail client"""
+
+    def test_auth_gmail_other_scopes(self):
+        """Test auth gmail with custom scopes"""
+
+    def test_make_gmail_client(self):
+        """Test it simply calls auth_gmail """
+
+    def test_auth_sheets(self):
+        """Test it gets credentials & builds a sheets client"""
+
+    def test_auth_sheets_other_scopes(self):
+        """Test auth sheets with custom scopes"""
 
     def test_run_query_provided_client(self):
         """Test run query with provided client"""
 
+    def test_run_query_no_client(self):
+        """Test run query with no client"""
+
     def test_get_table(self):
         """Test get table"""
-
-    def test_get_table_environment_blob(self):
-        """Test loading service account blob from environment"""
 
     def test_create_table_from_dataframe(self):
         """Test create table from dataframe"""
@@ -46,29 +76,20 @@ class TestGoogle(unittest.TestCase):
     def test_create_table_from_dataframe_bad_types(self):
         """Test create table from dataframe with bad type"""
 
+    def test_get_table_for_loading(self):
+        """Test get table for loading"""
+
     def test_load_data_from_dataframe(self):
         """Test load data from dataframe"""
 
-    def test_auth_gcs(self):
-        """Test auth gcs"""
+    def test_load_data_from_list(self):
+        """Test load data from list"""
 
     def test_upload_data_to_gcs(self):
         """Test upload data to gcs"""
 
     def test_upload_data_to_gcs_already_exists(self):
-        """Test upload data to gcs"""
-
-    def test_make_gmail_client(self):
-        """Test make gmail client"""
-
-    def test_make_gmail_client_other_scopes(self):
-        """Test make gmail client with custom scopes"""
-
-    def test_auth_sheets(self):
-        """Test auth sheets"""
-
-    def test_auth_sheets_other_scopes(self):
-        """Test auth sheets with custom scopes"""
+        """Test upload data to gcs when file already exists"""
 
     def test_get_data_from_sheets(self):
         """Test get data from sheest"""
