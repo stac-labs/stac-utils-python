2707539cdb7062fff2ffdfb4eac86993f03cb1a2
Micheál Keane <ffaristocrat@gmail.com>
2023-05-24 13:07:08 -0700

    start in on browser

--- src/tests/browser/test_downloader.py
+++ src/tests/browser/test_downloader.py
@@ -1,5 +1,6 @@
 import unittest
-from tempfile import TemporaryDirectory
+from unittest.mock import MagicMock, patch, call
+
 
 from src.stac_utils.browser.downloader import Downloader
 
@@ -17,8 +18,19 @@ class TestDownloader(unittest.TestCase):
     def test_clear(self):
         """Test clear"""
 
+        test_downloader = Downloader(".")
+        test_downloader.returned_files.add("Foo")
+        test_downloader.clear()
+        self.assertEqual(len(test_downloader.returned_files), 0)
+
     def test_check_for_new_files(self):
         """Test check for new files"""
 
+    def test_check_for_new_files_with_provided_pattern(self):
+        """Test check for new files when alternate pattern is provided"""
+
     def test_check_for_new_files_all_finished(self):
         """Test check for new files when we've consumed all files"""
+
+    def test_check_for_new_files_still_downloading(self):
+        """Test check for new files when a file is still downloading"""
--- src/tests/browser/test_driver.py
+++ src/tests/browser/test_driver.py
@@ -1,4 +1,5 @@
 import unittest
+from unittest.mock import MagicMock, patch, call
 
 from src.stac_utils.browser.driver import ChromeDriver
 
