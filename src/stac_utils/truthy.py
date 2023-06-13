f579a5d1c839f77384e9d7e59ed2d2af2b763a6c
Shannon Cwikla <socwikla@gmail.com>
2023-06-13 13:53:58 -0700

    updated docstring based on feedback

--- src/stac_utils/truthy.py
+++ src/stac_utils/truthy.py
@@ -3,7 +3,8 @@ TRUE_VALUES = ["1", "T", "TRUE", "Y", "YES"]
 
 def truthy(thing: str, true_values: list = None) -> bool:
     """
-    Evaluates whether a string is `True`
+    Evaluates whether a string is `True`. All of the following will evaluate as `True`: 1, Yes, Y, Yeah, T, True, true.
+    Most other things will evaluate as `False`, including `None` and `undefined`.
 
     :param thing: Item to evaluate
     :param true_values: Optional list of alternate `True` values
