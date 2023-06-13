f18ce30d3e3f442a4d95c047977dbd4c050ecbf6
Shannon Cwikla <socwikla@gmail.com>
2023-06-12 18:44:17 -0700

    docstrings

--- src/stac_utils/time.py
+++ src/stac_utils/time.py
@@ -9,21 +9,33 @@ DEFAULT_TIMEZONE = "America/New_York"
 
 
 def parse_date_for_tz(value: str) -> datetime:
-    """Parse date adjusting for timezone if timezone is not present"""
+    """
+    Parse date adjusting for timezone if timezone is not present
+
+    :return: Parsed date (adjusted for timezone if no timezone present)
+    """
     timezone = gettz(os.environ.get("TIMEZONE", DEFAULT_TIMEZONE))
 
     return default_tzinfo(_parse(value), timezone)
 
 
 def get_today_tz() -> datetime:
-    """Shortcut for getting today's date in the timezone specified by the 'TIMEZONE' environment variable"""
+    """
+    Shortcut for getting today's date in the timezone specified by the 'TIMEZONE' environment variable
+
+    :return: Today's date in timezone specified by environment variable
+    """
     timezone = gettz(os.environ.get("TIMEZONE", DEFAULT_TIMEZONE))
 
     return _today(timezone)
 
 
 def get_now_tz() -> datetime:
-    """Shortcut for getting the current time in the timezone specified by the 'TIMEZONE' environment variable"""
+    """
+    Shortcut for getting the current time in the timezone specified by the 'TIMEZONE' environment variable
+
+    :return: Current time in timezone specified by environment variable
+    """
     timezone = gettz(os.environ.get("TIMEZONE", DEFAULT_TIMEZONE))
 
     return datetime.now(timezone)
