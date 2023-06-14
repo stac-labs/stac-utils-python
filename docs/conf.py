c3f4c7e3b8506408439dba4460347f8b69239e3a
bxjw <bella@staclabs.io>
2023-06-14 10:48:16 -0400

    import stac_utils?

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,10 @@ release = "0.1"
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 import os
 import sys
+
 sys.path.insert(0, os.path.abspath('../src'))
+
+import stac_utils
 
 extensions = [
     "sphinx.ext.napoleon",
