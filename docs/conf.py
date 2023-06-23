89c3648ce6228c70e5eac0b697174a41b6a04b32
bxjw <bella@staclabs.io>
2023-06-23 13:48:59 -0400

    specify python 3.9 github action

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ import sys
 sys.path.insert(0, os.path.abspath('../src'))
 
 import stac_utils
-from stac_utils import email
+import stac_utils.email
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
