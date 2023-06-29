c8c07f83f3e7457e6f48de7ff8561640e0163aa1
Shannon Cwikla <socwikla@gmail.com>
2023-06-29 14:22:45 -0700

    fix up reach class

--- src/stac_utils/reach.py
+++ src/stac_utils/reach.py
@@ -1,98 +1,48 @@
-import logging
-import os, sys
 import json
-import pandas as pd
+import logging
+import os
+
 import requests
 
-from datetime import date, timedelta
-from io import StringIO
-
-from stac_utils.google import auth_bq, run_query
+from stac_utils.http import HTTPClient
 
 logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.INFO)
-
-if "REACH_API_USER" not in os.environ:
-    # Try to load from .env
-    from dotenv import load_dotenv
-
-    state = "local"
-    load_dotenv()
-
-try:
-    reach_keys = os.environ["REACH_API_USER"]
-except KeyError:
-    sys.exit("Reach API keys environment variable not set, cannot run report")
-
-query_base = "SELECT myv_van_id FROM "
 
 
-def get_api_headers():
-    auth_info = {
-        "username": os.environ["REACH_API_USER"],
-        "password": os.environ["REACH_API_PASSWORD"],
-    }
-    r = requests.post("https://api.reach.vote/oauth/token", data=auth_info)
-    access_token = {"Authorization": "Bearer " + json.loads(r.text)["access_token"]}
-    return access_token
+class ReachClient(HTTPClient):
+    base_url = "https://api.reach.vote/api/v1"
 
+    def __init__(self, api_user: str = None, api_password: str = None, *args, **kwargs):
+        self.api_user = api_user or os.environ["REACH_API_USER"]
+        self.api_password = api_password or os.environ["REACH_API_PASSWORD"]
+        self.access_token = None
+        super().__init__(*args, **kwargs)
 
-def update_people_to_tag(row, van_ids, tag_id, api_call_headers, remove=False):
-    if remove is True:
-        payload = {
-            "people": [
-                {
-                    "person_id": x,
-                    "person_id_type": "Voterfile VAN ID",
-                    "action": "removed",
-                }
-                for x in van_ids
-            ]
+    def refresh_auth(self, response: requests.Response):
+        body = {
+            "username": self.api_user,
+            "password": self.api_password,
         }
-    else:
-        payload = {
-            "people": [
-                {"person_id": x, "person_id_type": "Voterfile VAN ID"} for x in van_ids
-            ]
+        endpoint = "/oauth/token"
+        with self.session_context() as session:
+            response = session.post(self.base_url + endpoint, body=body)
+            self.access_token = json.loads(response.text)["access_token"]
+
+    def create_session(self) -> requests.Session:
+        """Create a session, set headers & auth"""
+        if not self.access_token:
+            self.refresh_auth(None)
+
+        session = requests.Session()
+        headers = {"Authorization": "Bearer " + self.access_token}
+        session.headers.update(headers)
+        return session
+
+    def get_api_headers(self):
+        auth_info = {
+            "username": os.environ["REACH_API_USER"],
+            "password": os.environ["REACH_API_PASSWORD"],
         }
-    r = requests.put(
-        "https://api.reach.vote/api/v1/tags/" + tag_id,
-        data=json.dumps(payload),
-        headers=api_call_headers,
-    )
-    if r.status_code == 401:
-        api_call_headers = get_api_headers(row)
-        r = requests.put(
-            "https://api.reach.vote/api/v1/tags/" + tag_id,
-            data=json.dumps(payload),
-            headers=api_call_headers,
-        )
-    return r
-
-
-def bq_to_reach(table_name, tag_name):
-    client = auth_bq()
-
-    query_string = query_base + " " + table_name
-    voters = run_query(client, query_string).myv_van_id.to_list()
-
-    print(voters)
-
-    api_call_headers = get_api_headers()
-    r = requests.get("https://api.reach.vote/api/v1/tags", headers=api_call_headers)
-    all_tags = json.loads(r.text)["tags"]
-
-    print(all_tags)
-
-    id = [x["id"] for x in all_tags if x["name"] == tag_name][0]
-
-    print(id)
-
-    if len(voters) > 0:
-        batches = [
-            voters[i * 1000 : (i + 1) * 1000]
-            for i in range((len(voters) + 1000 - 1) // 1000)
-        ]
-        for batch in batches:
-            put = update_people_to_tag(batch, id, api_call_headers)
-            logger.info("Updating Voters with Tag: " + tag_name)
+        r = requests.post("https://api.reach.vote/oauth/token", data=auth_info)
+        access_token = {"Authorization": "Bearer " + json.loads(r.text)["access_token"]}
+        return access_token
