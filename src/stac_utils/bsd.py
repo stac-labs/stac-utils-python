d6525e519a062bc5fbe68b53db83ef5180818dac
Shannon Cwikla <socwikla@gmail.com>
2023-06-29 13:50:25 -0700

    bsd client

--- src/stac_utils/bsd.py
+++ src/stac_utils/bsd.py
@@ -1,145 +1,90 @@
-import hmac, hashlib
-import os, time
-import sys
+import hashlib
+import hmac
+import os
+import time
 
-import xmltodict
 import requests
-import pandas as pd
+import xmltodict
 
-from insert_leads import insert_hot_lead
-from datetime import datetime, date, timedelta
 from stac_utils.http import HTTPClient
 
 
-
-
-
-signup_form_id = "115"  # NC Volunteer Form ID from BSD
-
 class BSDClient(HTTPClient):
     """"""
+
     api_ver = 2  # BSD API ID - always 2
 
-    def __init__(self, bsd_url: str = None, bsd_api_id: str = None, bsd_api_secret: str = None, *args, **kwargs):
-
-        self.bsd_url = bsd_url or os.environ['BSD_URL']
-        self.bsd_api_id = bsd_api_id or os.environ['BSD_API_ID']
-        self.bsd_api_secret = bsd_api_secret or os.environ['BSD_API_SECRET']
+    def __init__(
+        self,
+        bsd_url: str = None,
+        bsd_api_id: str = None,
+        bsd_api_secret: str = None,
+        *args,
+        **kwargs,
+    ):
+        self.bsd_url = bsd_url or os.environ["BSD_URL"]
+        self.bsd_api_id = bsd_api_id or os.environ["BSD_API_ID"]
+        self.bsd_api_secret = bsd_api_secret or os.environ["BSD_API_SECRET"]
 
         super().__init__(*args, **kwargs)
 
-    def generate_api_mac(self, current_time: str, url: str, params_str: str = None):
-        params = f'api_ver=2&api_id={self.bsd_api_id}&api_ts={current_time}'
+    def generate_api_mac(self, current_time: str, url: str, params: dict = None):
+        params_str = f"api_ver=2&api_id={self.bsd_api_id}&api_ts={current_time}"
 
-        if params_str is not None:
-            params += "&" + params_str
+        if params:
+            for k, v in params.items():
+                params_str += f"&{k}={v}"
 
-        signing_str = self.bsd_api_id + os.linesep \
-                      + current_time + os.linesep \
-                      + url + os.linesep \
-                      + params
+        signing_str = (
+            self.bsd_api_id
+            + os.linesep
+            + current_time
+            + os.linesep
+            + url
+            + os.linesep
+            + params_str
+        )
 
-        api_mac = hmac.new(self.bsd_api_secret.encode(), signing_str.encode(), hashlib.sha1).hexdigest()
+        api_mac = hmac.new(
+            self.bsd_api_secret.encode(), signing_str.encode(), hashlib.sha1
+        ).hexdigest()
 
         return api_mac
 
-
-    # Get volunteer signups from the NC Volunteer Form from the previous day.
-    # Returns a pandas DataFrame containing sign in data.
-    def get_volunteer_signups(self):
+    def call_api(
+        self,
+        method: str,
+        endpoint: str,
+        params: dict = None,
+        body: dict = None,
+        return_headers: bool = False,
+        use_snake_case: bool = True,
+        override_error_logging: bool = False,
+        override_data_printing: bool = False,
+        **kwargs,
+    ):
         current_time = str(int(time.time()))
+        api_mac = self.generate_api_mac(current_time, endpoint, params)
 
-        url = '/page/api/signup/get_signups_by_form_id'
-        params_str = "signup_form_id=" + signup_form_id
-
-        columns = ['ID', 'First Name', 'Last Name', 'Phone', 'Email', 'Zip', 'VR', 'Canvass', 'Calls', 'Texts',
-                   'Attend Events', 'Donate', 'Signup Date']
-        api_mac = generate_api_mac(current_time, url, params_str)
-
-        params = {
-            'api_ver': api_ver,
-            'api_id': BSD_API_ID,
-            'api_ts': current_time,
-            'api_mac': api_mac,
-            'signup_form_id': signup_form_id
+        new_params = {
+            "api_ver": self.api_ver,
+            "api_id": self.bsd_api_id,
+            "api_ts": current_time,
+            "api_mac": api_mac,
         }
 
-        today = datetime.now().date()
+        new_params.update(**params)
+        super().call_api(
+            method,
+            endpoint,
+            params=new_params,
+            body=body,
+            return_headers=return_headers,
+            use_snake_case=use_snake_case,
+            override_error_logging=override_error_logging,
+            override_data_printing=override_data_printing,
+            **kwargs,
+        )
 
-        resp = requests.get(get_full_url(url), params=params)
-
-        if resp.status_code == requests.codes.ok:
-            signup_data = []
-            print(resp.text)
-
-            # Response from BSD is an XML structure so we need to parse it
-            xml = xmltodict.parse(resp.text, xml_attribs=False)
-            signups = xml['api']['stg_signups']['stg_signup']
-
-            for signup in signups:
-                s_data = {
-                    'ID': signup['stg_signup_id'],
-                    'First Name': signup['firstname'].title() if signup['firstname'].title() else None,
-                    'Last Name': signup['lastname'].title() if signup['lastname'] else None,
-                    'Phone': signup['phone'],
-                    'Email': signup['email'].lower() if signup['email'] else None,
-                    'Zip': signup['zip'],
-                    'VR': False,
-                    'Canvass': False,
-                    'Calls': False,
-                    'Texts': False,
-                    'Attend Events': False,
-                    'Donate': False,
-                    'Signup Date': signup['create_dt'].split(" ")[0]
-                }
-
-                t = s_data['Signup Date'].split("-")
-                d = date(int(t[0]), int(t[1]), int(t[2]))
-
-                yesterday = today - timedelta(days=1)
-
-                if (d == yesterday):
-                    if 'stg_signup_extra' in signup:
-                        su_extra = signup['stg_signup_extra']
-
-                        if 'stg_signup_extra_value' in su_extra:
-                            extra_vals = su_extra['stg_signup_extra_value']
-
-                            if isinstance(extra_vals, list):
-                                for extra_val in extra_vals:
-                                    value = extra_val['value_varchar']
-
-                                    if (value == 'Help register voters'):
-                                        s_data['VR'] = True
-                                    elif (value == 'Canvass'):
-                                        s_data['Canvass'] = True
-                                    elif (value == 'Make calls'):
-                                        s_data['Calls'] = True
-                                    elif (value == 'Text voters'):
-                                        s_data['Texts'] = True
-                                    elif (value == 'Attend events'):
-                                        s_data['Attend Events'] = True
-                                    elif (value == 'Donate'):
-                                        s_data['Donate'] = True
-                            else:
-                                value = extra_vals['value_varchar']
-
-                                if (value == 'Help register voters'):
-                                    s_data['VR'] = True
-                                elif (value == 'Canvass'):
-                                    s_data['Canvass'] = True
-                                elif (value == 'Make calls'):
-                                    s_data['Calls'] = True
-                                elif (value == 'Text voters'):
-                                    s_data['Texts'] = True
-                                elif (value == 'Attend events'):
-                                    s_data['Attend Events'] = True
-                                elif (value == 'Donate'):
-                                    s_data['Donate'] = True
-
-                    signup_data.append(s_data)
-
-            signup_df = pd.DataFrame(data=signup_data, columns=columns)
-            print(signup_df)
-
-            return signup_df
+    def transform_response(self, response: requests.Response, **kwargs):
+        return xmltodict.parse(response.text, xml_attribs=False)
