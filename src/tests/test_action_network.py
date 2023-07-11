ecd816233b09462249b6e6d97f9c7341c9465def
Shannon Cwikla <socwikla@gmail.com>
2023-07-11 15:05:16 -0700

    add some doc strings

--- src/tests/test_action_network.py
+++ src/tests/test_action_network.py
@@ -10,6 +10,7 @@ class TestActionNetworkClient(unittest.TestCase):
         self.test_client = ActionNetworkClient()
 
     def test_init_env_keys(self):
+        """Test that client initializes with environmental keys"""
         test_api_token = "foo"
 
         with patch.dict(os.environ, values={"ACTIONNETWORK_API_TOKEN": test_api_token}):
@@ -17,6 +18,7 @@ class TestActionNetworkClient(unittest.TestCase):
             self.assertEqual(test_api_token, test_client.api_token)
 
     def test_create_session(self):
+        """Make sure API token and content type set in headers"""
         test_client = ActionNetworkClient("foo")
         self.assertTrue(
             {
@@ -27,6 +29,7 @@ class TestActionNetworkClient(unittest.TestCase):
         )
 
     def test_transform_response(self):
+        """Test that response is transformed and includes status code"""
         mock_data = {"foo_bar": "spam"}
         mock_response = MagicMock()
         mock_response.status_code = 42
@@ -36,5 +39,6 @@ class TestActionNetworkClient(unittest.TestCase):
         self.assertEqual({"foo_bar": "spam", "status_code": 42}, test_data)
 
     def test_check_response_for_rate_limit(self):
+        """Test that response has rate limit of 1"""
         test_client = ActionNetworkClient("foo")
         self.assertEqual(1, test_client.check_response_for_rate_limit(None))
