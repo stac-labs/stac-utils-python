849c8ba18e9eb5d791a0d45333e8058e09bb219a
bxjw <bella@staclabs.io>
2023-12-18 09:23:52 -0500

    add transitions

--- src/stac_utils/jira.py
+++ src/stac_utils/jira.py
@@ -19,8 +19,6 @@ class JiraClient(HTTPClient):
     base_url: organization's Jira URL, will pick up "JIRA_BASE_URL" if it's in the environment
     """
 
-    base_service_desk_endpoint = "rest/servicedeskapi/servicedesk"
-
     def __init__(
         self,
         api_user: str = None,
@@ -59,24 +57,36 @@ class JiraClient(HTTPClient):
 
         return data
 
-    # def get_jira_issue_url(self, issue_key: str) -> str:
-    #     return f"{self.base_url}/rest/api/3/issue/{issue_key}/"
+    def get_issue_url(self, issue_key: str) -> str:
+        return f"rest/api/3/issue/{issue_key}/"
 
-    def get_service_desks(self) -> dict:
-        return self.get(self.base_service_desk_endpoint, override_data_printing=True)
+    def get_issue(self, issue_key: str, **kwargs) -> dict:
+        self.get(self.get_issue_url(issue_key), **kwargs)
 
-    def get_issue_types(self, project_id: str) -> list[dict]:
+    def get_issue_transitions(self, issue_key: str, **kwargs) -> dict:
+        self.get(f"{self.get_issue_url(issue_key)}/transitions", **kwargs)
+
+    def get_issue_types(self, project_id: str, **kwargs) -> list[dict]:
         return self.get(
-            "rest/api/3/issuetype/project",
-            params={"projectId": project_id},
-            override_data_printing=True,
+            "rest/api/3/issuetype/project", params={"projectId": project_id}, **kwargs
+        )
+
+    def get_service_desks(self, **kwargs) -> dict:
+        return self.get("rest/servicedeskapi/servicedesk", **kwargs)
+
+    def post_issue_transitions(self, issue_key: str, transition_id: int) -> dict:
+        return self.post(
+            f"{self.get_issue_url(issue_key)}/transitions",
+            body={"transition": {"id": transition_id}},
         )
 
     def find_or_create_user(
         self, email_address: str, first_name: str, last_name: str
     ) -> int:
         existing_user = self.get(
-            "rest/api/3/user/search", params={"query": email_address}
+            "rest/api/3/user/search",
+            params={"query": email_address},
+            override_data_printing=True,
         )
 
         if existing_user and len(existing_user) > 0:
@@ -88,7 +98,7 @@ class JiraClient(HTTPClient):
                 "email": email_address,
                 "displayName": f"{first_name} {last_name}",
             },
-            override_data_printing=True
+            override_data_printing=True,
         )
 
         return new_user.get("accountId")
@@ -104,7 +114,7 @@ class JiraClient(HTTPClient):
         issue_type_lookup: str = "Task",
         reporter_id: int = None,
     ) -> dict:
-        service_desk_response = self.get_service_desks()
+        service_desk_response = self.get_service_desks(override_data_printing=True)
         service_desk = [
             board
             for board in service_desk_response.get("values")
@@ -118,7 +128,9 @@ class JiraClient(HTTPClient):
 
         service_desk_id = service_desk[0].get("projectId")
 
-        issue_type_response = self.get_issue_types(service_desk_id)
+        issue_type_response = self.get_issue_types(
+            service_desk_id, override_data_printing=True
+        )
         issue_type = [
             it for it in issue_type_response if it.get("name") == issue_type_lookup
         ]
@@ -147,46 +159,30 @@ class JiraClient(HTTPClient):
     def post_create_issue(self, payload: dict) -> dict:
         return self.post("rest/api/3/issue", body=payload)
 
+    def send_transition_to_jira_by_name(self, issue_key: str, to_status_name: str):
+        issue_response = self.get_issue(issue_key, override_data_printing=True)
+        if (
+            issue_response.get("fields", {}).get("status", {}).get("name")
+            == to_status_name
+        ):
+            logger.info("Ticket already in correct status")
+            return
 
-# def send_transition_to_jira_by_name(issue_key, to_status_name):
-#     base_url = get_jira_issue_url(issue_key)
-#     url = f"{base_url}/transitions"
-#     headers = get_jira_headers()
+        transition_list_response = self.get_issue_transitions(
+            issue_key, override_data_printing=True
+        )
+        transitions = transition_list_response.get("transitions", [])
 
-#     try:
-#         issue_response = requests.get(base_url, headers=headers).json()
-#         if (
-#             issue_response.get("fields", {}).get("status", {}).get("name")
-#             == to_status_name
-#         ):
-#             print("Ticket already in correct status")
-#             return
-#     except Exception as e:
-#         raise e
+        transition = [
+            t for t in transitions if t.get("to", {}).get("name") == to_status_name
+        ]
 
-#     try:
-#         transition_list_response = requests.get(url, headers=headers).json()
-#         transitions = transition_list_response.get("transitions", [])
+        if len(transition) < 1:
+            raise ValueError(
+                f"Could not find a transition to {to_status_name} for {issue_key}"
+            )
 
-#         for transition in transitions:
-#             transition_to_name = transition["to"]["name"]
-#             if transition_to_name == to_status_name:
-#                 transition_id = int(transition["id"])
-#                 return send_transition_to_jira(issue_key, transition_id)
-
-#         raise ValueError(f"No transition named {to_status_name} in list {transitions}")
-#     except Exception as e:
-#         print(f"Unable to find transition to {to_status_name} on issue {issue_key}")
-#         raise e
-
-
-# def send_transition_to_jira(issue_key, jira_transition_id):
-#     body_data = {"transition": {"id": jira_transition_id}}
-#     base_url = get_jira_issue_url(issue_key)
-#     url = f"{base_url}/transitions"
-#     headers = get_jira_headers()
-
-#     response = requests.post(url, json=body_data, headers=headers)
-
-#     if response.status_code != 204:
-#         raise ValueError(f"Error sending transition to Jira: {response.status_code}")
+        return self.post_issue_transitions(
+            issue_key,
+            int(transition[0].get("id")),
+        )
