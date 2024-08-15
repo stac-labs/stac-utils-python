18246ab7dbe59a09380a93dbdb7b69309deef01e
Jeff Rose <jeff@staclabs.io>
2024-08-15 16:00:25 -0400

    try some more browser tricks

--- src/stac_utils/browser/driver.py
+++ src/stac_utils/browser/driver.py
@@ -60,13 +60,16 @@ class ChromeDriver:
         options.add_argument("--window-size=1280x1696")
         options.add_argument("--disable-dev-shm-usage")
         options.add_argument("--disable-dev-tools")
+        options.add_argument("--disable-blink-features=AutomationControlled") 
         options.add_argument("--no-zygote")
+        options.add_argument("--disable-dev-tools")
         options.add_argument(f"--user-data-dir={tempfile.mkdtemp()}")
         options.add_argument(f"--data-path={tempfile.mkdtemp()}")
         options.add_argument(f"--disk-cache-dir={tempfile.mkdtemp()}")
         options.add_experimental_option(
             "prefs", {"download.default_directory": self.download_directory}
         )
+        options.add_experimental_option("useAutomationExtension", False) 
 
         self.driver = webdriver.Chrome(service=service, options=options)
 
@@ -74,6 +77,8 @@ class ChromeDriver:
             so it can be referenced by the script
         """
         self.driver.download_directory = self.download_directory
+
+        self.driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})") 
 
         return self.driver
 
